888381842a65821502516ae1795dd0be9327e760
Michael-Scrumlord <125718990+Michael-Scrumlord@users.noreply.github.com>
2023-03-28 19:36:03 -0700

    General cleanup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import BookList from './components/BookList';
-import InfoCard from './components/InfoCard';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
