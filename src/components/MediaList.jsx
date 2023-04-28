77d58666a6e22a2c47ae46c0ce5e1ae42eb0ddbb
Michael-Scrumlord <125718990+Michael-Scrumlord@users.noreply.github.com>
2023-04-28 00:34:56 -0700

    More cleanups

--- src/components/MediaList.jsx
+++ src/components/MediaList.jsx
@@ -1,112 +1,93 @@
-import InfoCard from './InfoCard'
 import { MediaData } from './MediaData'
+import StarBasedRating from 'star-based-rating'
+
+function MediaPoster (props) {
+  return(
+    <img src={props.mediaposter} className="movie-poster" alt="">
+    </img>
+  )
+}
+
+function TitleCard(props) {
+  return(
+    <div className="movie-titlecard">
+      <h1>{props.title} ({props.year})</h1>
+      <p>{props.genre}</p>
+      <StarRating rating={props.rating}/>
+      <Synopsis synopsis={props.synopsis}/>
+    </div>
+  )
+}
+
+// https://www.npmjs.com/package/star-based-rating //
+function StarRating(props) {
+return (
+  <>
+    <StarBasedRating 
+      totalStars={10}
+      previousStarsToDisplay={props.rating} />     
+  </>
+)
+}
+
+function Synopsis (props) {
+  return (
+    <div className="synopsis">
+      <p className="synopsisField">{props.synopsis}</p>
+    </div>
+  )
+}
+function InfoCard (props) {
+  return (
+      <div className="movie-card">
+        <MediaPoster mediaposter={props.mediaposter}/>
+        <TitleCard {...props}/>
+      </div>
+  )
+}
 
 export default function MediaList(props) {
-    var i = 0;
+    
+    var mediaType = 0;
+    var listSize = 10
+
     // References the index in MediaData[i][j]
     switch (props.media) {
       case "Movie":
-        i = 0;
+        mediaType = 0;
         break;
 
       case "Series":
-        i = 1;
+        mediaType = 1;
         break;
 
       case "Book":
-        i = 2;
+        mediaType = 2;
         break;
       
       case "Game":
-        i = 3;
+        mediaType = 3;
         break;
     
       default:
         break;
     }
-    // I should probably do something about this...
-    return(
-      <div className="content">
-                <InfoCard media={props.media} 
-                  title={MediaData[i][0].Name} 
-                  synopsis={MediaData[i][0].Synopsis} 
-                  mediaposter={MediaData[i][0].Poster}
-                  genre={MediaData[i][0].Genre}
-                  rating={MediaData[i][0].UserRating}
-                  year={MediaData[i][0].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][1].Name} 
-                  synopsis={MediaData[i][1].Synopsis} 
-                  mediaposter={MediaData[i][1].Poster}
-                  genre={MediaData[i][1].Genre}
-                  rating={MediaData[i][1].UserRating}
-                  year={MediaData[i][1].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][2].Name} 
-                  synopsis={MediaData[i][2].Synopsis} 
-                  mediaposter={MediaData[i][2].Poster}
-                  genre={MediaData[i][2].Genre}
-                  rating={MediaData[i][2].UserRating}
-                  year={MediaData[i][2].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][3].Name} 
-                  synopsis={MediaData[i][3].Synopsis} 
-                  mediaposter={MediaData[i][3].Poster}
-                  genre={MediaData[i][3].Genre}
-                  rating={MediaData[i][3].UserRating}
-                  year={MediaData[i][3].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][4].Name} 
-                  synopsis={MediaData[i][4].Synopsis} 
-                  mediaposter={MediaData[i][4].Poster}
-                  genre={MediaData[i][4].Genre}
-                  rating={MediaData[i][4].UserRating}
-                  year={MediaData[i][4].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][5].Name} 
-                  synopsis={MediaData[i][5].Synopsis} 
-                  mediaposter={MediaData[i][5].Poster}
-                  genre={MediaData[i][5].Genre}
-                  rating={MediaData[i][5].UserRating}
-                  year={MediaData[i][5].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][6].Name} 
-                  synopsis={MediaData[i][6].Synopsis} 
-                  mediaposter={MediaData[i][6].Poster}
-                  genre={MediaData[i][6].Genre}
-                  rating={MediaData[i][6].UserRating}
-                  year={MediaData[i][6].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][7].Name} 
-                  synopsis={MediaData[i][7].Synopsis} 
-                  mediaposter={MediaData[i][7].Poster}
-                  genre={MediaData[i][7].Genre}
-                  rating={MediaData[i][7].UserRating}
-                  year={MediaData[i][7].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][8].Name} 
-                  synopsis={MediaData[i][8].Synopsis} 
-                  mediaposter={MediaData[i][8].Poster}
-                  genre={MediaData[i][8].Genre}
-                  rating={MediaData[i][8].UserRating}
-                  year={MediaData[i][8].Year}
-                />
-                <InfoCard media={props.media} 
-                  title={MediaData[i][9].Name} 
-                  synopsis={MediaData[i][9].Synopsis} 
-                  mediaposter={MediaData[i][9].Poster}
-                  genre={MediaData[i][9].Genre}
-                  rating={MediaData[i][9].UserRating}
-                  year={MediaData[i][9].Year}
-                />
-      </div>
+
+    const mediaList= [];
+
+  for (let rank = 0; rank < listSize; rank++) {
+        mediaList.push (<InfoCard media={props.media} 
+          title={MediaData[mediaType][rank].Name} 
+          synopsis={MediaData[mediaType][rank].Synopsis} 
+          mediaposter={MediaData[mediaType][rank].Poster}
+          genre={MediaData[mediaType][rank].Genre}
+          rating={MediaData[mediaType][rank].UserRating}
+          year={MediaData[mediaType][rank].Year}
+          />  )
+  }
+    return (
+      <div className = "content">
+          {mediaList} 
+        </div>
     )
   }
