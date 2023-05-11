50cac7723f8a97133eb92a769679c71612dd6d60
Michael-Scrumlord <125718990+Michael-Scrumlord@users.noreply.github.com>
2023-05-10 23:39:14 -0700

    Updating router

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import "bootstrap/dist/js/bootstrap.min.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <HashRouter> 
       <App /> 
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
